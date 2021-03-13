ccbf08f0d90ef47ea949d1003bc81b8f6d1eac72
Moldir Tokhtarbek <moldirtokhtarbek@MacBook-Pro-Moldir.local>
2021-03-13 17:38:54 +0600

    add all file

---
+++ js/index.js
@@ -0,0 +1,34 @@
+console.log("Hello World!!!");
+console.log('Hello World!!!');
+console.log(24);
+console.log("Hello " + "Duisen!");
+console.log(20 + 25);
+console.log(20 * 20);
+console.log("20 * 20");
+console.log("20" + 20); // "20" + 20 => "20" + "20" = "2020"
+console.log(20**2);
+console.log(Math.pow(20, 2));
+console.log(20 / 20);
+console.log(20 % 15);
+console.log(10 % 5);
+
+var num1 = 20;
+console.log(num1); 
+// console.log(num1 + 10); // num1 + 10 => 20 + 10 = 30
+
+num1 = num1 - 10; // num1 = num1 - 10 => num1 = 20 - 10 => num1 = 10;
+
+console.log(num1);
+
+num1 = num1 + num1;
+
+console.log(num1);
+
+var num2 = 35;
+
+var num3 = num1;
+
+num1 = num2;
+num2 = num3;
+
+console.log(num1, num2);
